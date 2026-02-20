9d8591a63883a94e445f9e1b9040dbbe01ff5bb2
Bauyrjan18 <baukabalkybek@gmail.com>
2026-02-20 12:15:04 +0500

    2

--- src/com/narxoz/rpg/combat/FireBreath.java
+++ src/com/narxoz/rpg/combat/FireBreath.java
@@ -2,15 +2,11 @@ package com.narxoz.rpg.combat;
 
 public class FireBreath implements Ability {
     @Override
-    public String getName(){
-        return "FireBreath";
-    }
+    public String getName(){return "FireBreath";}
     @Override
     public String getDescription(){return "Spews fire at the enemy";}
     @Override
     public int getDamage() {return 50;}
     @Override
-    public Ability clone(){
-        return new FireBreath();
-    }
+    public Ability clone(){return new FireBreath();}
 }
--- src/com/narxoz/rpg/combat/IceSpike.java
+++ src/com/narxoz/rpg/combat/IceSpike.java
@@ -4,9 +4,7 @@ public class IceSpike implements Ability {
     @Override
     public int getDamage() {return 30;};
     @Override
-    public Ability clone(){
-        return new IceSpike();
-    }
+    public Ability clone(){return new IceSpike();}
     @Override
     public String getDescription() {return "Throws a sharp ice spike";};
     @Override
