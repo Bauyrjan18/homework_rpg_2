45f8009923049cfc1c9cc7a78c94469db854a337
Bauyrjan18 <baukabalkybek@gmail.com>
2026-02-21 18:39:48 +0500

    8

--- src/com/narxoz/rpg/factory/EnemyComponentFactory.java
+++ src/com/narxoz/rpg/factory/EnemyComponentFactory.java
@@ -7,5 +7,4 @@ import java.util.List;
 public interface EnemyComponentFactory {
     List<Ability> createAbilities();
     LootTable createLootTable();
-    String createAIBehavior();
 }
---
+++ src/com/narxoz/rpg/factory/FireComponentFactory.java
@@ -0,0 +1,19 @@
+package com.narxoz.rpg.factory;
+
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.combat.FireBreath;
+import com.narxoz.rpg.loot.FireLoot;
+import com.narxoz.rpg.loot.LootTable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FireComponentFactory implements EnemyComponentFactory {
+    @Override
+    public List<Ability> createAbilities() {
+        List<Ability> list = new ArrayList<>();
+        list.add(new FireBreath());
+        return list;
+    }
+    @Override
+    public LootTable createLootTable() { return new FireLoot(); }
+}
